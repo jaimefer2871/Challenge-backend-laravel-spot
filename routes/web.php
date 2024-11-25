874eb5fec4fbc76dde56d5c92b566e842ab81792
Jaime Fernandez <jaimefer2871@gmail.com>
2024-11-25 13:37:04 -0400

    2dos cambios

--- routes/web.php
+++ routes/web.php
@@ -17,28 +17,26 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => 'api'], function () use ($router) {
-    $router->group(['prefix' => 'v1', 'namespace' => 'V1'], function () use ($router) {
-        $router->group(['prefix' => 'urlshortener'], function () use ($router) {
-            $router->get('/', [
-                'as' => 'urlshortener-index',
-                'uses' => 'UrlShortenerController@index'
-            ]);
+$router->group(['prefix' => 'v1', 'namespace' => 'V1'], function () use ($router) {
+    $router->group(['prefix' => 'urlshortener'], function () use ($router) {
+        $router->get('/', [
+            'as' => 'urlshortener-index',
+            'uses' => 'UrlShortenerController@index'
+        ]);
 
-            $router->get('/{id}', [
-                'as' => 'urlshortener-show',
-                'uses' => 'UrlShortenerController@show'
-            ]);
+        $router->get('/{id}', [
+            'as' => 'urlshortener-show',
+            'uses' => 'UrlShortenerController@show'
+        ]);
 
-            $router->post('/', [
-                'as' => 'urlshortener-create',
-                'uses' => 'UrlShortenerController@create'
-            ]);
+        $router->post('/', [
+            'as' => 'urlshortener-create',
+            'uses' => 'UrlShortenerController@create'
+        ]);
 
-            $router->delete('/{id}', [
-                'as' => 'urlshortener-delete',
-                'uses' => 'UrlShortenerController@delete'
-            ]);
-        });
+        $router->delete('/{id}', [
+            'as' => 'urlshortener-delete',
+            'uses' => 'UrlShortenerController@delete'
+        ]);
     });
 });
